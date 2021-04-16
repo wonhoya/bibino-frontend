8fa9c8d68696c5703eab71313654173160b5a830
yobe8 <sanyobe@gmail.com>
2021-04-16 11:38:15 +0900

    Add a base structure for development

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Start!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
