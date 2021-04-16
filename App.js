791b86141d3a706b1a20530dbb0db8a593540e76
yobe8 <sanyobe@gmail.com>
2021-04-16 20:04:04 +0900

    delete example files

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
