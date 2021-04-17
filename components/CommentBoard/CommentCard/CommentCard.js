00a4d093a6af141691e399515e512ef0995b850b
yobe8 <sanyobe@gmail.com>
2021-04-17 21:40:07 +0900

    Add comment card

---
+++ components/CommentBoard/CommentCard/CommentCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Image, Text, Dimensions } from "react-native";
+import avatarImg from "../../../assets/pngs/avatarSample.png";
+import styles from "./styles";
+
+import RatingBoard from "../../shared/RatingBoard/RatingBoard";
+
+const userMock = {
+  id: 1,
+  username: "Juhyung",
+  work: "Software engineer",
+  rating: 4,
+  comment: "와 이거 완전 시원!",
+};
+
+const { width } = Dimensions.get("window");
+
+const CommentCard = (user) => {
+  const { id, username, work, rating, comment } = userMock;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image id={id} style={styles.image} source={avatarImg} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.userName}>{username}</Text>
+        <Text style={styles.work}>{work}</Text>
+        <Text style={styles.comment}>{comment}</Text>
+      </View>
+      <View style={styles.ratingBoardContainer}>
+        <RatingBoard mode="static" rating={rating} size={width / 20} />
+      </View>
+    </View>
+  );
+};
+
+export default CommentCard;
---
+++ components/CommentBoard/CommentCard/styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Dimensions } from "react-native";
+import {
+  RUBIK_MEDIUM,
+  RUBIK_BOLD,
+  RUBIK_REGULAR,
+} from "../../../constants/font";
+
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    width: width / 1.2,
+    height: width / 4.8,
+    borderRadius: 5,
+    backgroundColor: "#FFFFFF",
+    flexDirection: "row",
+  },
+  imageContainer: {
+    flex: 0.8,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    marginLeft: "2%",
+  },
+  image: {
+    width: width / 6,
+    height: width / 6,
+    borderRadius: 50,
+    borderWidth: 2,
+  },
+  textContainer: {
+    flex: 1.5,
+    marginTop: "1%",
+    marginLeft: "3%",
+  },
+  userName: {
+    fontFamily: RUBIK_BOLD,
+    fontSize: 18,
+    marginBottom: "2%",
+  },
+  work: {
+    fontFamily: RUBIK_MEDIUM,
+    fontSize: 11,
+    marginBottom: "6%",
+  },
+  comment: {
+    fontFamily: RUBIK_REGULAR,
+    fontSize: 16,
+  },
+  ratingBoardContainer: {
+    flex: 1,
+    marginTop: "1.5%",
+    marginRight: "4%",
+  },
+});
+
+export default styles;
