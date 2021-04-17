116948de0daef54291242e6af96c25e2ffd4a2d9
Wonho Lee <biocle8339@gmail.com>
2021-04-17 14:26:51 +0900

    ADD main simple style

--- screens/Main/styles.js
+++ screens/Main/styles.js
@@ -1,9 +1,29 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform, StatusBar, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
   font: {
     fontFamily: "Rubik_700Bold",
     fontSize: 50,
+  },
+  androidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  screen: {
+    width: Dimensions.get("window").width * 0.91,
+    alignSelf: "center",
+  },
+  profileContainer: {
+    marginTop: 10,
+  },
+  upperProfileContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: Dimensions.get("window").height * 0.2,
+  },
+  hashTagContainer: {},
+  contentsContainer: {
+    height: Dimensions.get("window").height * 0.4,
   },
 });
 
