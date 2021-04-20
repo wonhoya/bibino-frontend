1766d7d8baad911820ed8d46156fca472228893d
JuhyoungJUNG <fheh1992@gmail.com>
2021-04-20 19:20:17 +0900

    Add navigation to resultpage

--- screens/Photo/Photo.js
+++ screens/Photo/Photo.js
@@ -1,65 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Image } from "react-native";
+import { View, Text, Image } from "react-native";
 import { Camera } from "expo-camera";
 
 import styles from "./styles";
 import Configuration from "../Configuration/Configuration";
 import PhotoTabBar from "../../components/PhotoTabBar/PhotoTabBar";
 import * as FileSystem from "expo-file-system";
-import { API_URL } from "@env";
+import callGoogleVisionAsync from "../../utils/callGoogleVisionAsync";
 
-async function callGoogleVisionAsync(image) {
-  const body = {
-    requests: [
-      {
-        image: {
-          content: image,
-        },
-        features: [
-          { type: "LABEL_DETECTION", maxResults: 10 },
-          { type: "TEXT_DETECTION", maxResults: 5 },
-          { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
-          { type: "WEB_DETECTION", maxResults: 5 },
-        ],
-      },
-    ],
-  };
-
-  const response = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-  const parsed = await response.json();
-
-  console.log("Result:", parsed);
-
-  return parsed.responses[0].labelAnnotations[0].description;
-}
+import { useNavigation } from "@react-navigation/native";
 
 const Photo = () => {
   const cameraRef = useRef(null);
-  //카메라 허가
+  const navigation = useNavigation();
+
   const [hasPermission, setHasPermission] = useState(null);
-  //카메라 타입체크 후면 카메라가 기본으로 사용
-  const [type, setType] = useState(Camera.Constants.Type.back);
+
   //카메라가 실행되어야만 takepicture해야함
   const [isCameraReady, setisCameraReady] = useState(false);
-
   const [isPreview, setIsPreview] = useState(false);
-
+  const [isParseStarted, setIsParseStarted] = useState(false);
   const [photoUri, setPhotoUri] = useState(null);
-
   const [photobase64, setPhotobase64] = useState(null);
-
-  const [test, setTest] = useState(null);
 
   console.log("Permission check", hasPermission);
   console.log("Camera Ready check", isCameraReady);
-  console.log("test is", test);
 
   useEffect(() => {
     const requestPermission = async () => {
@@ -72,10 +37,10 @@ const Photo = () => {
       }
     };
     requestPermission();
-  }, [hasPermission]);
+  }, []);
 
   useEffect(() => {
-    const readPhotos = async () => {
+    const makeDirectory = async () => {
       //폴더가 없으면 폴더를 만들고, 있으면 거기 있는 사진을 불러옴.
       // await FileSystem.deleteAsync(FileSystem.documentDirectory + "photos/");
 
@@ -93,16 +58,16 @@ const Photo = () => {
           FileSystem.documentDirectory + "photos/"
         );
 
-        //uri에 주소 붙이기
+        //uri에 주소 붙이기 이미지 불러오기 위해서는 이미지 이름 앞에 ${FileSystem.documentDirectory}photos/
         const uris = photos.map((photo) => {
           return `${FileSystem.documentDirectory}photos/${photo}`;
         });
 
-        setTest(uris);
+        console.log(uris);
       }
     };
 
-    readPhotos();
+    makeDirectory();
   }, []);
 
   const handleTakePicture = async () => {
@@ -113,12 +78,12 @@ const Photo = () => {
     if (cameraRef.current) {
       const option = { quality: 1, base64: true };
       const photoData = await cameraRef.current.takePictureAsync(option);
-      console.log("photoData is", photoData);
-      console.log(photoData.base64);
-      setPhotobase64(photoData.base64);
-      setPhotoUri(photoData.uri);
+      // console.log("photoData is", photoData);
+      // console.log(photoData.base64);
       await cameraRef.current.pausePreview();
       setIsPreview(true);
+      setPhotobase64(photoData.base64);
+      setPhotoUri(photoData.uri);
     }
   };
 
@@ -132,17 +97,20 @@ const Photo = () => {
       //디렉토리 삭제
       // await FileSystem.deleteAsync(FileSystem.documentDirectory + "photos/");
 
+      setIsParseStarted(true);
       await FileSystem.copyAsync({
         from: photoUri,
         to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
       });
-
-      console.log("photobase64:", photobase64);
-
-      callGoogleVisionAsync(photobase64);
-
       // 구글 비전으로 보내기
+      const parsedText = await callGoogleVisionAsync(photobase64);
+      console.log("parsedText:", parsedText);
+      setIsParseStarted(false);
+
+      // 여기서 navigate
+      navigation.navigate();
     } catch (error) {
+      navigation.navigate();
       console.log(error);
     }
   };
@@ -153,29 +121,40 @@ const Photo = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.test}>
-        {test &&
-          test.map((photo, index) => {
-            return (
-              <Image style={styles.image} key={index} source={{ uri: photo }} />
-            );
-          })}
-      </View>
-      <Camera
-        ref={cameraRef}
-        style={styles.camera}
-        type={type}
-        onCameraReady={() => setisCameraReady(true)}
-      />
-      <PhotoTabBar
-        handleTakePicture={handleTakePicture}
-        handleRetake={handleRetake}
-        handleUse={handleUse}
-        isPreview={isPreview}
-      />
-    </View>
+    <>
+      {isParseStarted ? (
+        <Text>Parse Started</Text>
+      ) : (
+        <View style={styles.container}>
+          <Camera
+            ref={cameraRef}
+            style={styles.camera}
+            type={Camera.Constants.Type.back}
+            onCameraReady={() => setisCameraReady(true)}
+          />
+          <PhotoTabBar
+            handleTakePicture={handleTakePicture}
+            handleRetake={handleRetake}
+            handleUse={handleUse}
+            isPreview={isPreview}
+          />
+        </View>
+      )}
+    </>
   );
 };
 
 export default Photo;
+
+{
+  /* <View style={styles.test}>
+{test &&
+  test.map((photo, index) => {
+    return (
+      <Image style={styles.image} key={index} source={{ uri: photo }} />
+    );
+  })}
+</View> */
+}
+
+// const [test, setTest] = useState(null);
---
+++ utils/callGoogleVisionAsync.js
@@ -0,0 +1,35 @@
+import { API_URL } from "@env";
+
+async function callGoogleVisionAsync(image) {
+  const body = {
+    requests: [
+      {
+        image: {
+          content: image,
+        },
+        features: [
+          { type: "LABEL_DETECTION", maxResults: 10 },
+          { type: "TEXT_DETECTION", maxResults: 5 },
+          { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
+          { type: "WEB_DETECTION", maxResults: 5 },
+        ],
+      },
+    ],
+  };
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const parsed = await response.json();
+
+  console.log("Result:", parsed);
+
+  return parsed.responses[0].labelAnnotations[0].description;
+}
+
+export default callGoogleVisionAsync;
