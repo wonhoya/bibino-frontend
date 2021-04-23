a2b018ba98b675bc4ae381e24ff8aa37f65b64d2
yobe8 <sanyobe@gmail.com>
2021-04-24 03:21:54 +0900

    Add util function and Clean codes

--- screens/Search/Search.js
+++ screens/Search/Search.js
@@ -1,41 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { SafeAreaView, View, Text, TextInput } from "react-native";
 import { useSelector } from "react-redux";
-import ModalDropdown from "react-native-modal-dropdown";
-import debounce from "lodash.debounce";
-import { BACKEND_URL_FOR_DEV } from "@env";
 
 import styles from "./styles";
 import { SearchIcon } from "../../assets/svgs/icon";
 import { SEARCH_TITLE_TEXT } from "../../constants/text";
-import getHeadersIncludedIdToken from "../../utils/getHeadersIncludedIdToken";
+import debouncedFetchBeerSearched from "../../utils/debouncedFetchBeerSearched";
 import SearchCardBoard from "../../components/SearchCardBoard/SearchCardBoard";
 
 //mockup
 //https://github.com/RazaShehryar/react-native-modal-dropdown#api
 //onSelect <= interaction func
-
-const fetchBeerSearched = async (searchText, idToken, setSearchedBeers) => {
-  const headers = getHeadersIncludedIdToken(idToken);
-  const response = await fetch(`${BACKEND_URL_FOR_DEV}/beers/search`, {
-    method: "POST",
-    headers: { ...headers, "Content-Type": "application/json" },
-    body: JSON.stringify({ searchText }),
-  });
-
-  const searchedBeers = await response.json();
-  const processedSearchedBeers = searchedBeers.map((beer) => {
-    return {
-      id: beer._id,
-      name: beer.name,
-      description: beer.description.slice(0, 25) + "...",
-      imagePath: beer.imagePath,
-    };
-  });
-  setSearchedBeers(processedSearchedBeers);
-};
-
-const debouncedFetchBeerSearched = debounce(fetchBeerSearched, 300);
 
 const Search = () => {
   const idToken = useSelector((state) => state.token.idToken);
