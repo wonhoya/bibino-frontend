dc45fd53db79d719d8dc396e0ee3c252f4faaedf
yobe8 <sanyobe@gmail.com>
2021-04-27 01:44:01 +0900

    Add error handling

--- utils/fetchSearchBeers.js
+++ utils/fetchSearchBeers.js
@@ -1,28 +1,33 @@
 import debounce from "lodash.debounce";
 import { BACKEND_URL_FOR_DEV } from "@env";
 
+import showErrorInDevelopment from "./showErrorInDevelopment";
 import generateHeaderOption from "./generateHeaderOption";
 
 const fetchSearchBeers = debounce(
   async (searchText, idToken, setSearchedBeers) => {
-    const headers = generateHeaderOption(idToken);
-    const response = await fetch(
-      `${BACKEND_URL_FOR_DEV}/beers/search/?text=${encodeURI(searchText)}`,
-      {
-        headers,
-      }
-    );
-
-    const searchedBeers = await response.json();
-    const managedSearchedBeers = searchedBeers.map((beer) => {
-      return {
-        id: beer._id,
-        name: beer.name,
-        description: beer.description.slice(0, 25) + "...",
-        imagePath: beer.imagePath,
-      };
-    });
-    setSearchedBeers(managedSearchedBeers);
+    try {
+      const headers = generateHeaderOption(idToken);
+      const response = await fetch(
+        `${BACKEND_URL_FOR_DEV}/beers/search/?text=${encodeURI(searchText)}`,
+        {
+          headers,
+        }
+      );
+      const searchedBeers = await response.json();
+      const managedSearchedBeers = searchedBeers.map((beer) => {
+        return {
+          id: beer._id,
+          name: beer.name,
+          description: beer.description.slice(0, 25) + "...",
+          imagePath: beer.imagePath,
+        };
+      });
+      setSearchedBeers(managedSearchedBeers);
+    } catch (err) {
+      showErrorInDevelopment("Faild search beer ", err);
+      setSearchedBeers([]);
+    }
   },
   300
 );
